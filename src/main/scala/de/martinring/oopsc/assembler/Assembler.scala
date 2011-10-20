d21177fde777e9b1edfad7e6acebb3ae00f5556a
Martin Ring <mail@martinring.de>
2011-10-20 14:09:48 +0200

    improved code generation

--- src/main/scala/de/martinring/oopsc/assembler/Assembler.scala
+++ src/main/scala/de/martinring/oopsc/assembler/Assembler.scala
@@ -1,37 +1,74 @@
 package de.martinring.oopsc
 
 object Assembler {
-  private def instruction(name: String, a: Any, b: Any): String = "%s %s, %s;".format(name, a.toString, b.toString)
-   
+  def label(s: String) = Instruction(s + ":")  
+  
+  trait Instr {
+    var comment: String = ""
+    def || (comment: String) = {
+      this.comment = "; " + comment
+      this
+    }
+  }
+      
+  case class Instruction(name: String, args: Any*) extends Instr {    
+    override def toString = name + " " + args.mkString(", ") + comment
+  }
+  
+  case object No extends Instr {
+    override def toString = comment
+  }
+  
+  case class Label(label: String) extends Instr {
+    override def toString = label + ": " + comment
+  }
+      
+  case class Instructions(title: String)(is: Instr*) extends Instr {    
+    override def toString = "; " + title + "\n" + is.mkString("\n") + "\n" + "; END " + title
+  }   
+  
   object O
 
-  case class R(x: Int) {
-    def := (n: Int) = instruction("MRI", this, n)
-    def := (Ry: R) = instruction("MRR", this, Ry)
-    def := (m: Address) = instruction("MRM ", this, m)
-    def := (label: String) = instruction("MRI", this, label)
-    def := (c: Condition) = c match {
-      case ISZ(ry) => instruction("ISZ", this, ry)
-      case ISP(ry) => instruction("ISP", this, ry)
-      case ISN(ry) => instruction("ISN", this, ry)
+  sealed abstract class R(x: Int) {
+    def << (n: Int) = Instruction("MRI", this, n)
+    def << (Ry: R) = Instruction("MRR", this, Ry)
+    def << (m: Address) = Instruction("MRM ", this, m)
+    def << (label: String) = Instruction("MRI", this, label)
+    def << (c: Condition) = c match {
+      case ISZ(ry) => Instruction("ISZ", this, ry)
+      case ISP(ry) => Instruction("ISP", this, ry)
+      case ISN(ry) => Instruction("ISN", this, ry)
     }
-    def += (Ry: R) = instruction("ADD", this, Ry)
-    def -= (Ry: R) = instruction("SUB", this, Ry)
-    def *= (Ry: R) = instruction("MUL", this, Ry)
-    def /= (Ry: R) = instruction("DIV", this, Ry)
-    def %= (Ry: R) = instruction("MOD", this, Ry)
-    def &= (Ry: R) = instruction("AND", this, Ry)
-    def |= (Ry: R) = instruction("OR", this, Ry)
-    def ^= (Ry: R) = instruction("XOR", this, Ry)
+    def <<+ (Ry: R) = Instruction("ADD", this, Ry)
+    def <<- (Ry: R) = Instruction("SUB", this, Ry)
+    def <<* (Ry: R) = Instruction("MUL", this, Ry)
+    def <</ (Ry: R) = Instruction("DIV", this, Ry)
+    def <<% (Ry: R) = Instruction("MOD", this, Ry)
+    def <<& (Ry: R) = Instruction("AND", this, Ry)
+    def <<| (Ry: R) = Instruction("OR", this, Ry)
+    def <<^ (Ry: R) = Instruction("XOR", this, Ry)
     def === (o: O.type) = { ISZ(this) }
     def > (i: O.type) = { ISP(this) }
     def < (i: O.type) = { ISN(this) }
+    
+    def unary_~ = Address(this)
 
     override def toString = "R" + x
   }
-
-  object Address { def update(Rx: R, Ry: R) = instruction("MMR", Address(Rx), Ry) }
-  case class Address(address: R) { override def toString = "(" + address + ")" }
+  
+  object R0 extends R(0)
+  object R1 extends R(1)
+  object R2 extends R(2)
+  object R3 extends R(3)
+  object R4 extends R(4)
+  object R5 extends R(5)
+  object R6 extends R(6)
+  object R7 extends R(7)
+ 
+  case class Address(address: R) { 
+    def << (Ry: R) = Instruction("MMR", this, Ry)
+    override def toString = "(" + address + ")" 
+  }
 
   trait Condition
   case class ISZ(Ry: R) extends Condition
--- src/main/scala/de/martinring/oopsc/assembler/Code.scala
+++ src/main/scala/de/martinring/oopsc/assembler/Code.scala
@@ -8,102 +8,84 @@ import de.martinring.oopsc.Transform._
 /*
  * Code generation
  */
-object  Code {
-  val lf = "\n"
-  def lines(ls: String*) = ls.mkString(lf)
-  def indent(ls: String) = ls.split(lf).map("  " + _).mkString(lf)  
-  def section(title: String)(content: String*): String = {
-    ";;; " + title + " ;;;\n" + 
-    indent(lines(content :_*))
-  }
-  
-  implicit def pimpString(s: String) = new {
-    def |> (comment: String) = s + " " + comment
-  }
-
+object  Code {  
   val stack = "_stack"
   val heap = "_heap"
   val end = "_end"
 
-  def generate(element: Element): Transform[String] = element match {
+  def generate(element: Element): Transform[Instr] = element match {
     case Program(main) => for {
       mainClass <- generate(main)
       main      <- generate(Access(New("Main"), "main"))      
-    } yield lines(
-      "; Start-Code: initialize registers",
-      ( R(1) := 1     ) |> "R1 is allways 1",
-      ( R(2) := stack ) |> "R2 points to stack",
-      ( R(4) := heap  ) |> "R4 points to next free position on Heap",
-      ( indent(main)  ),
-      ( R(0) := end   ) |> "exit program",
-      ( indent(mainClass) ),
-      ( stack + ":;" |> "Start of stack" ),
-      ( "DAT " + App.stackSize + ", 0;" ),
-      ( heap + ":;" |> "Start of heap" ),
-      ( "DAT " + App.heapSize + ", 0;" ),
-      ( end + ":;" |> "End of program" ))
+    } yield Instructions("Program")(
+      R1 << 1       || "R1 is allways 1",
+      R2 << stack   || "R2 points to stack",
+      R4 << heap    || "R4 points to next free position on Heap",
+      main,
+      R0 << end     || "exit program",
+      mainClass,
+      Label(stack)  || "Start of stack",
+      Instruction("DAT", App.stackSize, 0),
+      Label(heap)   || "Start of heap",
+      Instruction("DAT", App.heapSize, 0),
+      Label(end)    || "End of program")
 
     case c: Class => for {
       _       <- enter(c)
       methods <- sequence(c.methods map generate)
       _       <- leave
-    } yield section("CLASS " + c.name)(methods :_*)      
+    } yield Instructions("CLASS " + c.name)(methods :_*)
 
     case m: Method => for {
       _     <- enter(m)
       c     <- currentType
-      val label = c + "_" + m.name
+      val l = c + "_" + m.name
       body  <- sequence(m.body map generate)     
       _     <- leave
-    } yield lines(
-      section("METHOD " + m.name)(      
-        ( label + ":" ),
-        ( R(2) += R(1) ),
-        ( Address(R(2)) = R(3) ) |> "Save old stack frame",
-        ( R(3) := R(2) ) |> "Current position on stack is new frame",
-        ( if (!m.variables.isEmpty) lines (
-            ( R(5) := m.variables.size ),
-            ( R(2) += R(5) ) |> "Create space for local variables"
-          ) else "; Method has no variables"),
-        lines(body :_*)             
-      ),
-      ( R(5) := m.variables.size + 3 ),
-      ( R(2) -= R(5) ) |> "Correct stack",
-      ( R(3) -= R(1) ),
-      ( R(5) := Address(R(3))) |> "Get return address",
-      ( R(3) += R(1) ),
-      ( R(3) := Address(R(3))) |> "Get old stack frame",
-      ( R(0) := R(5) ) |> "return"
+    } yield Instructions("METHOD " + m.name)(      
+      label(l),
+      R2 <<+ R1,
+      ~R2 << R3 || "Save old stack frame",
+      R3 << R2  || "Current position on stack is new frame",
+      ( if (!m.variables.isEmpty) Instructions("Create space for local variables")(
+        R5 << m.variables.size,
+        R2 <<+ R5)
+        else No ),
+      Instructions("BODY")(body :_*),
+      R5 << m.variables.size + 3,
+      R2 <<- R5  || "Correct stack",
+      R3 <<- R1 ,
+      R5 << ~R3 || "Get return address",
+      R3 <<+ R1 ,
+      R3 << ~R3 || "Get old stack frame",
+      R0 << R5  || "return"
     )
     
     case Read(operand) => for {
         newInt  <- generate(New("Integer"))
         operand <- generate(operand)
-    } yield lines(
+    } yield Instructions("READ")(
       operand,
       newInt,
-      section("READ")(
-        ( R(5) := Address(R(2)) ),
-        ( R(6) := Class.headerSize ),
-        ( R(5) += R(6) ),
-        ( "SYS 0, 6" ),
-        ( Address(R(5)) = R(6) )  |> "Write character to new Integer",
-        ( R(5) := Address(R(2)) )   |> "Get new Int from stack",
-        ( R(2) -= R(1) ),
-        ( R(6) := Address(R(2)) ) |> "Take target from stack",
-        ( R(2) -= R(1) ),
-        ( Address(R(6)) = R(5) )  |> "Assign"
-      ))
+      R5 << ~R2 ,
+      R6 << Class.headerSize ,
+      R5 <<+ R6 ,
+      Instruction("SYS", 0, 6),
+      ~R5 << R6   || "Write character to new Integer",
+      R5 << ~R2    || "Get new Int from stack",
+      R2 <<- R1 ,
+      R6 << ~R2  || "Take target from stack",
+      R2 <<- R1 ,
+      ~R6 << R5   || "Assign"
+    )
       
     case Write(operand) => for {
       operand <- generate(operand)
-    } yield lines(
-      operand,
-      section("WRITE")(
-        R(5) := Address(R(2)),
-        R(2) -= R(1),
-        "SYS 1, 5"
-      )
+    } yield Instructions("WRITE")(
+      operand,      
+      R5 << ~R2,
+      R2 <<- R1,
+      Instruction("SYS", 1, 5)
     )
       
     case While(condition, body) => for {
@@ -111,183 +93,169 @@ object  Code {
       body <- sequence(body map generate)
       whileLabel <- nextLabel
       endLabel <- nextLabel
-    } yield lines(
-      section("WHILE")(
-        whileLabel + ":",
-        condition + ":",
-        ( R(5) := Address(R(2)) ) |> "Get condition from stack",
-        ( R(2) -= R(1) ),
-        ( "ISZ R5, R5;" ) |> "if 0 then",
-        ( "JPC R5, " + endLabel +";" ) |> "leave loop"      
-      ),
-      section("DO")(body :_*),
-      "MRI R0, " + whileLabel,
-      endLabel + ":")
+    } yield Instructions("WHILE")(
+      Instructions("CONDITION")(
+        Label(whileLabel),
+        condition,
+        R5 << ~R2  || "Get condition from stack",
+        R2 <<- R1,
+        R5 << (R5 === O) || "if 0 then",
+        Instruction("JPC", R5, endLabel) || "leave loop"),
+      Instructions("BODY")(body :_*),
+      R0 << whileLabel,
+      Label(endLabel))
       
     case If(condition, body) => for {
       condition <- generate(condition)
       body <- sequence(body map generate)
       endLabel <- nextLabel
-    } yield lines(
-      section("IF")(
+    } yield Instructions("IF")(
+      Instructions("CONDITION")(
         condition,
-        ( R(5) := Address(R(2)) ) |> "Get condition from stack",
-        ( R(2) -= R(1) ),
-        ( "ISZ R5, R5;" ) |> "if 0 then",
-        ( "JPC R5, " + endLabel +";" ) |> "jump to END IF"),
-      section("THEN")(body :_*),
-      endLabel + ":"
-      )
+        R5 << ~R2 || "Get condition from stack",
+        R2 <<- R1,
+        R5 << (R5 === O) || "if 0 then",
+        Instruction("JPC", R5, endLabel) || "jump to END IF"),
+      Instructions("THEN")(body :_*),
+      Label(endLabel))
       
     case Call(call) => for {
       call <- generate(call)
-    } yield section("CALL")(call)
+    } yield Instructions("CALL")(call)
       
     case Assign(left, right) => for {
       left <- generate(left)
       right <- generate(right)
-    } yield section("ASSIGNMENT")(
-      ( left ),
-      ( right ),
-      ( R(5) := Address(R(2)) ) |> "Take right value from stack",
-      ( R(2) -= R(1) ),
-      ( R(6) := Address(R(2)) ) |> "Take ref to left val from stack",
-      ( R(2) -= R(1) ),
-      ( Address(R(6)) = R(5) )  |> "Assign"
-    )    
+    } yield Instructions("ASSIGNMENT")(
+      left,
+      right,
+      R5 << ~R2  || "Take right value from stack",
+      R2 <<- R1,
+      R6 << ~R2  || "Take ref to left val from stack",
+      R2 <<- R1,
+      ~R6 << R5  || "Assign")    
 
     case Unary(operator, operand, typed) => for {      
       operand <- generate(operand)     
-    } yield lines(
-      operand,
-      section("UNARY " + operator)(
-        R(5) := Address(R(2)),
-        operator match {
-          case "-" => lines(
-            R(6) := 0,
-            R(6) -= R(5),
-            Address(R(2)) = R(6)
-          )
-        }
-      )
-    )
+    } yield Instructions("UNARY " + operator)(
+      operand,      
+      R5 << ~R2,
+      operator match {
+        case "-" => Instructions("")(
+          R6 << 0,
+          R6 <<- R5,
+          ~R2 << R6
+        )
+      })
     
     case Binary(operator, left, right, typed) => for {
       left <- generate(left)
       right <- generate(right)
-    } yield section("BINARY " + operator)(
+    } yield Instructions("BINARY " + operator)(
       left,
       right,
-      R(5) := Address(R(2)),
-      R(2) -= R(1),
-      R(6) := Address(R(2)),
+      R5 << ~R2,
+      R2 <<- R1,
+      R6 << ~R2,
       operator match {
-        case "+"   => R(6) += R(5)
-        case "-"   => R(6) -= R(5)
-        case "*"   => R(6) *= R(5)
-        case "/"   => R(6) /= R(5)
-        case "MOD" => R(6) %= R(5)
-        case ">"   => lines(
-          R(6) -= R(5),
-          "ISP R6, R6"
+        case "+"   => R6 <<+ R5
+        case "-"   => R6 <<- R5
+        case "*"   => R6 <<* R5
+        case "/"   => R6 <</ R5
+        case "MOD" => R6 <<% R5
+        case ">"   => Instructions("")(
+          R6 <<- R5,
+          R6 << (R6 > O)
         )
-        case ">="  => lines(
-          R(6) -= R(5),
-          "ISN R6, R6",
-          "XOR R6, R1"
+        case ">="  => Instructions("")(
+          R6 <<- R5,
+          R6 << (R6 < O),        
+          Instruction("XOR", R6, R1)
         )
-        case "<"   => lines(
-          R(6) -= R(5),
-          "ISN R6, R6"
+        case "<"   => Instructions("")(
+          R6 <<- R5,
+          R6 << (R6 < O)
         )
-        case "<="  => lines(
-          R(6) -= R(5),
-          "ISP R6, R6",
-          "XOR R6, R1"
+        case "<="  => Instructions("")(
+          R6 <<- R5,
+          R6 << (R6 > O),
+          Instruction("XOR", R6, R1)
         )
-        case "="   => lines(
-          R(6) -= R(5),
-          "ISZ R6, R6"
+        case "="   => Instructions("")(
+          R6 <<- R5,
+          R6 << (R6 === O) 
         )
-        case "#"   => lines(
-          R(6) -= R(5),
-          "ISZ R6, R6",
-          "XOR R6, R1"
+        case "#"   => Instructions("")(
+          R6 <<- R5,
+          R6 << (R6 === O),
+          Instruction("XOR", R6, R1)
         )
       },
-      Address(R(2)) = R(6)
+      ~R2 << R6
     ) 
           
     case Literal(value, typed) => success{
-      section(value + ": " + typed.name)(
-        R(5) := value,
-        R(2) += R(1),
-        Address(R(2)) = R(5)
-      )
-    }
+      Instructions(value + ": " + typed.name)(
+        R5 << value,
+        R2 <<+ R1,
+        ~R2 << R5
+      )}
     
     case New(typed) => for {
       clazz <- resolveClass(typed)
-    } yield section("NEW " + typed.name)(
-        R(2) += R(1),
-        (Address(R(2)) = R(4))   |> "Put reference to new object on stack",
-        R(5) := clazz.size.get,
-        (R(4) += R(5))           |> "Inc heap"
+    } yield Instructions("NEW " + typed.name)(
+        R2 <<+ R1,
+        ~R2 << R4   || "Put reference to new object on stack",
+        R5 << clazz.size.get,
+        R4 <<+ R5     || "Inc heap"
       )
     
     case Access(left, right, typed, lvalue) => for {
       l     <- generate(left)
       t     <- resolveClass(left.typed)      
       r     <- generateMember(t, right)
-    } yield lines(l,r)
+    } yield Instructions("ACCESS")(l,r)
       
     case Name(name, typed, lvalue) => for {      
       decl   <- resolve(name)
     } yield decl match {
-        case Variable(name, typed, offset) => section("REF TO VARIABLE " + name)(
-            R(5) := offset,
-            R(5) += R(3),
-            R(2) += R(1),
-            Address(R(2)) = R(5)
-          )
-        case x => error(x.toString)            
+        case Variable(name, typed, offset) => Instructions("REF TO VARIABLE " + name)(
+            R5 << offset,
+            R5 <<+ R3,
+            R2 <<+ R1,
+            ~R2 << R5)        
     }
       
     case Box(expr, typed) => for {
       newType <- generate(New(typed))
       expr    <- generate(expr)
-    } yield section("BOX")(
+    } yield Instructions("BOX")(
       newType,
       expr,
-      ( R(5) := Address(R(2)) )     |> "Take value from stack",
-      ( R(2) -= R(1) ),
-      ( R(6) := Address(R(2)) )     |> "Get reference to new object (stays on the stack)",
-      ( R(7) := Class.headerSize ),
-      ( R(6) += R(7) )              |> "Determine location in new object",
-      ( Address(R(6)) = R(5) )      |> "Write value to object"
-    )
+      R5 << ~R2      || "Take value from stack",
+      R2 <<- R1,
+      R6 << ~R2      || "Get reference to new object (stays on the stack)",
+      R7 << Class.headerSize,
+      R6 <<+ R7        || "Determine location in new object",
+      ~R6 << R5      || "Write value to object")
 
     case UnBox(expr, typed) => for {
       expr <- generate(expr)
-    } yield section("UNBOX")(
+    } yield Instructions("UNBOX")(
       expr,
-      ( R(5) := Address(R(2)) ) |> "Get reference to object from stack",
-      ( R(6) := Class.headerSize ),
-      ( R(5) += R(6) )          |> "Determine address of value",
-      ( R(5) := Address(R(5)) ) |> "Read value",
-      ( Address(R(2)) = R(5) )  |> "Write to stack"
-    )
+      R5 << ~R2  || "Get reference to object from stack",
+      R6 << Class.headerSize,
+      R5 <<+ R6    || "Determine address of value",
+      R5 << ~R5  || "Read value",
+      ~R2 << R5    || "Write to stack")
 
     case DeRef(expr, typed) => for {
       expr <- generate(expr)
-    } yield section("DEREF")(
+    } yield Instructions("DEREF")(
       expr,
-      R(5) := Address(R(2)),
-      R(5) := Address(R(5)),
-      Address(R(2)) = R(5)
-    )   
-    
-    case x => success{x.toString}
+      R5 << ~R2,
+      R5 << ~R5,
+      ~R2 << R5)           
   }
   
   def generateMember(t: Class, expr: Expression) = for {
@@ -296,21 +264,19 @@ object  Code {
     case Name(name, typed, lvalue) => 
       val decl = t.members.find(_.name == name).get
       decl match {
-        case Attribute(name, typed, offset) => section("REF TO ATTRIBUTE " + name)(
-            R(5) := Address(R(2)),
-            R(6) := offset,
-            R(5) += R(6),
-            Address(R(2)) = R(5)
-          )
+        case Attribute(name, typed, offset) => 
+          Instructions("REF TO ATTRIBUTE " + name)(
+            R5 << ~R2,
+            R6 << offset,
+            R5 <<+ R6,
+            ~R2 << R5)
         case Method(name, variables, body) => 
-          section("CALL OF METHOD " + name)(
-            R(5) := returnLabel,
-            R(2) += R(1),
-            Address(R(2)) = R(5),
-            "; static call of " + name,
-            R(0) := t.name + "_" + name,
-            returnLabel + ":"
-          )
+          Instructions("CALL OF METHOD " + name)(
+            R5 << returnLabel,
+            R2 <<+ R1,
+            ~R2 << R5,            
+            R0 << t.name + "_" + name,
+            Label(returnLabel))
         }
     }  
 }
